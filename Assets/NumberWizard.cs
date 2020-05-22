480f6ee4412a590cf0e16bf5cb8b39f32712d95b
Luke McCann <developer-lukemccann@outlook.com>
2020-05-22 17:46:36 +0100

    Adds min and max variables

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    private int max = 1000;
+    private int min = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(getBanner());
-        Debug.Log(getUserNumber(0, 10));
+        Debug.Log(getUserNumber(min, max));
     }
 
     // Update is called once per frame
@@ -17,13 +20,32 @@ public class NumberWizard : MonoBehaviour
         
     }
 
-    string getUserNumber(int lowest, int highest) {
-        string template = "Pick a number between: {0} and {1}";
+    // Game Methods
+    private string getUserNumber(int lowest, int highest) {
+        string template = "Pick a number between: {0} and {1} inclusive.";
         string[] data = {lowest.ToString(), highest.ToString()};
         return string.Format(template, data);
     }
 
-    string getBanner() {
+    // Getter/Setters
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+    
+
+    private string getBanner() {
         return "Welcome to Wizard";
     }
 }
