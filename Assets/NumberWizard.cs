e0a3cf49990080becc79b15d1feeb2fc341ed648
Luke McCann <developer-lukemccann@outlook.com>
2020-05-22 18:23:16 +0100

    Adds C# XML documentation

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The main NumberWizard
+/// contains all methods for running NumberWizard
+/// </summary>
+/// <author>
+/// Luke McCann
+/// </author>
 public class NumberWizard : MonoBehaviour
 {
     private int max = 0;
@@ -15,7 +22,7 @@ public class NumberWizard : MonoBehaviour
     {
         this.max = 1000;
         this.min = 1;
-        
+
         Debug.Log(getBanner());
         Debug.Log(getUserNumber(min, max));
         Debug.Log(guess(getRandomIntBetween(min, max)));
@@ -34,12 +41,23 @@ public class NumberWizard : MonoBehaviour
         return string.Format(template, data);
     }
 
+    /// <summary>
+    /// Guesses the user input
+    /// </summary>
+    /// <param name="num">the number to guess</param>
+    /// <returns>string asking if number is higher or lower</returns>
     private string guess(int num) {
         string guess = num.ToString();
         string template = "Is your number higher or lower than: {0}?";
         return string.Format(template, guess);
     }
 
+    /// <summary>
+    /// Generate a random integer between min and max values.
+    /// </summary>
+    /// <param name="min">The minimum integer to generate (inclusive)</param>
+    /// <param name="max">The maximum integer to generate (inclusive)</param>
+    /// <returns>random between two integers</returns>
     private int getRandomIntBetween(int min, int max) {
         System.Random rand = new System.Random();
         return rand.Next(min, max);
