29a64fc7f6ecfbd9fa0c9029bfeac75d436daf9b
Luke McCann <developer-lukemccann@outlook.com>
2020-05-22 18:03:21 +0100

    Adds C# RandomIntegerGeneration

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    private int max = 1000;
-    private int min = 1;
+    private int max = 0;
+    private int min = 0;
+
+    private int guessMax = 0;
+    private int guessMin = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.max = 1000;
+        this.min = 1;
+        
         Debug.Log(getBanner());
         Debug.Log(getUserNumber(min, max));
+        Debug.Log(guess(getRandomIntBetween(min, max)));
     }
 
     // Update is called once per frame
@@ -25,6 +32,17 @@ public class NumberWizard : MonoBehaviour
         string template = "Pick a number between: {0} and {1} inclusive.";
         string[] data = {lowest.ToString(), highest.ToString()};
         return string.Format(template, data);
+    }
+
+    private string guess(int num) {
+        string guess = num.ToString();
+        string template = "Is your number higher or lower than: {0}?";
+        return string.Format(template, guess);
+    }
+
+    private int getRandomIntBetween(int min, int max) {
+        System.Random rand = new System.Random();
+        return rand.Next(min, max);
     }
 
     // Getter/Setters
@@ -43,8 +61,23 @@ public class NumberWizard : MonoBehaviour
     public void setMin(int min) {
         this.min = min;
     }
-    
 
+    public int getGuessMax() {
+        return guessMax;
+    }
+
+    public void setGuessMax(int max) {
+        this.guessMax = max;
+    }
+
+    public void setGuessMin(int min) {
+        this.guessMin = min;
+    }
+
+    public int getGuessMin() {
+        return guessMin;
+    }
+    
     private string getBanner() {
         return "Welcome to Wizard";
     }
