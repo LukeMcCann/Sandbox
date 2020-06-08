257c3f0aca3c9a80f1b7fe14c62152473e1325c6
Luke McCann <developer-lukemccann@outlook.com>
2020-06-08 21:03:11 +0100

    Add new sandbox test

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int lowestNumber;
+    [SerializeField] int highestNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
